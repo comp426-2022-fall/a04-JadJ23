dd442e21321e850b3db19e97b11ebac81db28a33
root <root@jad23-pc.localdomain>
2022-11-30 18:38:55 -0500

    3

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
 import minimist from "minimist";
 import express from "express";
-import {roll} from "./lib/roll.js";
+import { roll } from "./lib/roll.js";
 
-const vals = minimist(process.argv.slice(2));
-const port = vals.port || 5000;
-const app = express();
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
 
-app.use(express.urlencoded({extended: true}));
-app.length('/app/', (req, res) => {
+const application = express();
+application.use(express.urlencoded({extended: true}));
+
+application.get('/app/', (req, res) => {
     res.send('200 OK');
     res.status(200);
 })
-app.length('/app/roll/', (req, res) => {
-    res.send(roll(6,2,1));
+
+application.get('/app/roll/', (req, res) => {
+    res.send(roll(6, 2, 1));
     res.status(200);
 })
-app.post('/app/roll/', (req, res) => {
+
+application.post('/app/roll/', (req, res) => {
     res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
     res.status(200);
 })
-app.get('/app/roll/:sides/', (req, res) => {
+
+application.get('/app/roll/:sides/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), 2, 1));
     res.status(200);
 })
-app.get('/app/roll/:sides/:dice/', (req, res) => {
+
+application.get('/app/roll/:sides/:dice/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
     res.status(200);
 })
-app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+
+application.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
     res.status(200);
 })
-app.use((req, res) => {
+
+application.use((req, res) => {
 	res.send("404 NOT FOUND");
 	res.status(404);
 });
 
-app.listen(port);
+application.listen(port);
